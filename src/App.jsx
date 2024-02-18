a9fb27f244edc555c5f97deb54bccfa2f0102bff
Nitin Srivastva <nitinsrivastava2all@gmail.com>
2024-02-18 23:55:26 +0530

    Custom hooks and Inputbox component is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
+import {Inputbox} from './component/Inputbox.jsx';
+import {useCurrencyInfo} from './hooks/useCurrencyInfo';
 
 function App() {
 
+  
+
+
   return (
     <>
-    <div className="flex flex-wrap justify-center w-full h-screen bg-[url(.\images\img-2.jpg)] bg-cover text-black">
-       <div className="border-slate-400 border-2 flex flex-wrap justify-center my-auto opacity-70 w-[40%] h-[45%] bg-black rounded-3xl">
-        <div className="w-[90%] h-[40%] mt-4 bg-white rounded-3xl"></div>
-        <div className="w-[90%] h-[40%] bg-white rounded-3xl"></div>
-       </div>
-    </div>
+      <div className="flex flex-wrap justify-center w-full h-screen bg-[url(.\images\img-2.jpg)] bg-cover text-black">
+        <div className="border-slate-700 border-2  flex flex-wrap justify-center my-auto opacity-80 w-[40%] h-[50%] bg-black rounded-3xl">
+          <Inputbox label="From"/>
+          <Inputbox label="To" />
+          <button className="text-white w-[60%] px-2 mb-1 bg-blue-700 rounded-xl border-1 border-white ">Convert Currency</button>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/component/Inputbox.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./Inputbox.css";
+
+const Inputbox = ({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions = [],
+  selectCurrency = "usd",
+  amountDisable = false,
+  currencyDisable = false,
+}) => {
+  return (
+    <>
+      <div className="flex flex-row w-[90%] h-[35%] justify-around my-auto bg-white rounded-3xl">
+        <div className="mx-2 flex flex-col w-[90%] h-[70%] justify-center my-auto bg-white rounded-3xl">
+          <label className="font-semibold">{label}</label>
+          <input
+            type="number"
+            className="bg-black h-7 my-auto px-1 w-[90%] rounded-sm text-white"
+            placeholder="Enter the Amount"
+            value={amount}
+            onChange={(e) =>
+              onAmountChange && onAmountChange(Number(e.target.value))
+            }
+            disabled={amountDisable}
+          />
+        </div>
+        <div className="flex flex-col w-[35%] h-[70%] justify-around my-auto bg-white rounded-3xl">
+          <label className="font-semibold">Select Currency</label>
+          <select
+            className="bg-black h-7 text-white  w-[90%] my-auto rounded-sm opacity-100"
+            value={selectCurrency}
+            disabled={currencyDisable}
+            onChange={(e) => {
+              onCurrencyChange && onCurrencyChange(e.target.value);
+            }}
+          >
+            {currencyOptions.map((currency) => (
+              <option key={currency} value={currency}>
+                {currency}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Inputbox;
