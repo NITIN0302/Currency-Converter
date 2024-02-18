4eb5c7607bd66405aae539b7067ca2d027a8e53c
Nitin Srivastva <nitinsrivastava2all@gmail.com>
2024-02-19 00:25:40 +0530

    Customised hook for API call

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
-import {Inputbox} from './component/Inputbox.jsx';
-import {useCurrencyInfo} from './hooks/useCurrencyInfo';
+import Inputbox from './component/Inputbox.jsx';
+import useCurrencyInfo from './hooks/useCurrencyInfo';
 
 function App() {
 
-  
+  const [amount,setAmount] = useState(0);
+  const [from,setFrom] = useState("usd");
+  const [to,setTo] = useState("inr");
+  const [convertedAmount,setConvertedAmount] = useState(0);
 
+  const currencyInfo = useCurrencyInfo(from);
+  const options = Object.keys(currencyInfo);
 
   return (
     <>
@@ -13,7 +18,7 @@ function App() {
         <div className="border-slate-700 border-2  flex flex-wrap justify-center my-auto opacity-80 w-[40%] h-[50%] bg-black rounded-3xl">
           <Inputbox label="From"/>
           <Inputbox label="To" />
-          <button className="text-white w-[60%] px-2 mb-1 bg-blue-700 rounded-xl border-1 border-white ">Convert Currency</button>
+          <button className="text-white w-[60%] px-2 mb-1 bg-blue-700 rounded-xl border-1 border-white">Convert {from} to {to}</button>
         </div>
       </div>
     </>
--- src/component/Inputbox.jsx
+++ src/component/Inputbox.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./Inputbox.css";
 
 const Inputbox = ({
   label,
